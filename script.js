e7feb4c7b6607df9c284f264612a2ee94909785d
JehanKheller <jeh.khe@gmail.com>
2023-10-29 23:39:31 -0400

    Commit function CharacterLimit.

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 function ClearDisplayResult() {
   resultado.innerHTML = '0';
+
+  DisplayFormat();
 }
 
 function Backspace() {
-  let result = resultado.innerHTML;
+  var result = resultado.innerHTML;
   resultado.innerHTML = result.substring(0, result.length - 1);
 
   if (result.length == 1) {
     resultado.innerHTML = '0';
   }
+
+  DisplayFormat()
 }
 
 function Calcular() {
@@ -18,12 +22,12 @@ function Calcular() {
     resultado.innerHTML = eval(total);
   }
   else {
-    resultado.innerHTML = '?';
+    resultado.innerHTML = 'ERROR!';
   }
 }
 
 function Insert(num) {
-  let result;
+  var result;
 
   if (resultado.innerHTML == '0') {
     resultado.innerHTML = '';
@@ -31,5 +35,28 @@ function Insert(num) {
 
   result = resultado.innerHTML;
   resultado.innerHTML = result + num;
+
+  CharacterLimit();
+  DisplayFormat();
 }
 
+function DisplayFormat() {
+  if (resultado.innerHTML.length > 8) {
+    resultado.setAttribute("style", "font-size: 36px");
+  }
+  if (resultado.innerHTML.length <= 8) {
+    resultado.setAttribute("style", "font-size: 48px");
+  }
+}
+
+// Limita a quantidade de caracteres digitados.
+function CharacterLimit() {
+  // Cria uma variável (obj) que recebe um seletor (#resultado).
+  let obj = document.querySelectorAll("#resultado");
+  
+  obj.forEach((txt) => {
+    let textC = txt.textContent;
+    let txtFormated = textC.substr(0, 11);
+    txt.innerHTML = txtFormated;
+  });
+}
