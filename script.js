4db4d3baed028482e9c0782ec24a71641c7bfbbb
JehanKheller <jeh.khe@gmail.com>
2023-10-30 11:28:17 -0400

    Commit implementation of the calculation.

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+var operatorState = new Boolean(false);
+var operatorValue = '';
+
 function ClearDisplayResult() {
   resultado.innerHTML = '0';
+  resultado_historico.innerHTML = '0';
+  operatorState = false;
+  operatorValue = '';
 
   DisplayFormat();
 }
@@ -16,30 +22,83 @@ function Backspace() {
 }
 
 function Calcular() {
-  var total = resultado.innerHTML;
+  var total = resultado_historico.innerHTML;
 
   if (total) {
-    resultado.innerHTML = eval(total);
+    const findTerm = (term) => {
+      if (total.includes(term)) {
+        return total;
+      }
+    }
+
+    switch (total) {
+      case findTerm("-"):
+        resultado.innerHTML = parseFloat(total) - parseFloat(resultado.innerHTML);
+        break
+
+      case findTerm("+"):
+        resultado.innerHTML = parseFloat(total) + parseFloat(resultado.innerHTML);
+        break
+
+      case findTerm("x"):
+        resultado.innerHTML = parseFloat(total) * parseFloat(resultado.innerHTML);
+        break
+
+      case findTerm("÷"):
+        resultado.innerHTML = parseFloat(total) / parseFloat(resultado.innerHTML);
+        break
+    }
   }
   else {
     resultado.innerHTML = 'ERROR!';
   }
 }
 
-function Insert(num) {
-  var result;
-
+function Insert(numero) {
   if (resultado.innerHTML == '0') {
     resultado.innerHTML = '';
   }
 
-  result = resultado.innerHTML;
-  resultado.innerHTML = result + num;
+  resultado.innerHTML = resultado.innerHTML + numero;
+
+  if (operatorState == true) {
+    resultado.innerHTML = '';
+    resultado.innerHTML = resultado.innerHTML + numero;
+    operatorState = false;
+  }
+  
+  if (operatorValue == '=') {
+    operatorValue = '';
+    resultado_historico.innerHTML = '0';
+    resultado.innerHTML = '';
+    resultado.innerHTML = resultado.innerHTML + numero;
+  }
 
   CharacterLimit();
   DisplayFormat();
 }
 
+function InsertOperator(operator) {
+  operatorValue = operator;
+  
+  if (resultado_historico.innerHTML == '0') {
+    resultado_historico.innerHTML = '';
+    resultado_historico.innerHTML = resultado.innerHTML + operator;
+  }
+  else if (resultado_historico.innerHTML != '0' && resultado.innerHTML != '0') {
+    resultado_historico.innerHTML = resultado_historico.innerHTML + resultado.innerHTML + operator;
+  }
+  
+  if (operator == '=') {
+    Calcular();
+  }
+
+  operatorState = true;
+
+  CharacterLimit();
+}
+
+// Formata o tamanho da fonte caso o elemento p (#resultado) receba mais ou menos de 8 dígitos.
 function DisplayFormat() {
   if (resultado.innerHTML.length > 8) {
     resultado.setAttribute("style", "font-size: 36px");
@@ -49,7 +108,7 @@ function DisplayFormat() {
   }
 }
 
-// Limita a quantidade de caracteres digitados.
+// Limita a quantidade máxima de caracteres que o elemento p (#resultado) pode receber.
 function CharacterLimit() {
   // Cria uma variável (obj) que recebe um seletor (#resultado).
   let obj = document.querySelectorAll("#resultado");
